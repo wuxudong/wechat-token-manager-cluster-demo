bb11f59fe56eac52df0beb1f18a6ac394992e8bf
xudong82113 <xudong82113@gmail.com>
2017-12-11 19:48:40 +0800

    remove useless Exception

--- src/main/java/com/github/wuxudong/wechat/DemoMain.java
+++ src/main/java/com/github/wuxudong/wechat/DemoMain.java
@@ -4,12 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-import javax.script.ScriptException;
-
 @SpringBootApplication
 @EnableScheduling
 public class DemoMain {
-    public static void main(String[] args) throws ScriptException {
+    public static void main(String[] args) {
         SpringApplication.run(DemoMain.class);
     }
 }
